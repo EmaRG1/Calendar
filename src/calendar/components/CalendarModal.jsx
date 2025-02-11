8df6373454ec06713dd81b4f8d0dd786c74539a2
Emanuel <emanuelrojasguindan@gmail.com>
2025-02-10 22:31:45 -0300

    installed react date picker and created calendar modal

---
+++ src/calendar/components/CalendarModal.jsx
@@ -0,0 +1,107 @@
+import { addHours } from 'date-fns';
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
+const initialForm = {
+  title: 'Emanuel',
+  notes: 'Rojas',
+  start: new Date(),
+  end: addHours(new Date(), 2)
+}
+
+
+export const CalendarModal = () => {
+
+  const [isOpen, setIsOpen] = useState(true);
+
+  const [formValues, setFormValues] = useState(initialForm)
+
+
+  const onInputChange = ({target}) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value
+    })
+  }
+
+  const onCloseModal = () => {
+    console.log('cerrando modal')
+    setIsOpen(false);
+  }
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onCloseModal}
+      style={customStyles}
+      className='modal'
+      overlayClassName='modal-fondo'
+      closeTimeoutMS={200}
+    >
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container">
+
+          <div className="form-group mb-2">
+            <label>Fecha y hora inicio</label>
+            <input className="form-control" placeholder="Fecha inicio" /> 
+          </div>
+
+          <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+              <input className="form-control" placeholder="Fecha inicio" /> 
+          </div>
+
+          <hr />
+          <div className="form-group mb-2">
+              <label>Titulo y notas</label>
+              <input 
+                  type="text" 
+                  className="form-control"
+                  placeholder="Título del evento"
+                  name="title"
+            autoComplete="off"
+            value={formValues.title}
+            onChange={onInputChange}
+              />
+              <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+          </div>
+
+          <div className="form-group mb-2">
+              <textarea 
+                  type="text" 
+                  className="form-control"
+                  placeholder="Notas"
+                  rows="5"
+            name="notes"
+            value={formValues.notes}
+            onChange={onInputChange}
+              ></textarea>
+              <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+          </div>
+
+          <button
+              type="submit"
+              className="btn-block btn btn-outline-primary"
+          >
+              <i className="fa-save far"></i>
+              <span> Guardar</span>
+          </button>
+
+      </form>
+
+    </Modal>
+  )
+}
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,15 +2,16 @@ import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import { addHours } from 'date-fns'
-import { NavBar, CalendarEvent } from '../'  
+import { NavBar, CalendarEvent, CalendarModal } from '../'  
 
 import { localizer, getMessagesES } from '../../helpers'
+import { useState } from 'react'
 
 const events = [{
   title: 'Subida a Tafí',
   notes: 'Some notes',
   start: new Date(),
-  end: addHours(new Date(), 2),
+  end: addHours(new Date(), 1),
   user: {
     _id: '123',
     name: 'Emanuel'
@@ -19,25 +20,43 @@ const events = [{
 
 export const CalendarPage = () => {
 
-  const eventStyleGetter = (event, start, end, isSelected) => {
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
 
+   const eventStyleGetter = (event, start, end, isSelected) => {
+
+   }
+
+  const onDoubleClick = (event) => {
+    console.log( {doubleClick: event})
+  }
+  const onSelect = (event) => {
+    console.log('select:',event)
+  }
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
   }
   return (
     <>
       <NavBar />
       
       <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 'calc(100vh - 80px)' }}
-      messages={getMessagesES()}
-      eventPropGetter={eventStyleGetter}
+        localizer={localizer}
+        events={events}
+        defaultView={lastView}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
         components={{
           event: CalendarEvent
-      }}
-    />
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+      />
+      
+      <CalendarModal />
     </>
   )
 }
