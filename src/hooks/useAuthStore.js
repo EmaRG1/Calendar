5d7082657d8898b4c5d112b999337b47895b494e
Emanuel <emanuelrojasguindan@gmail.com>
2025-03-13 20:29:30 -0300

    register logic

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -14,7 +14,7 @@ export const useAuthStore = () => {
       const { data } = await calendarAPI.post('/auth', { email, password })
       localStorage.setItem('token', data.token);
       localStorage.setItem('token-init-date', new Date().getTime());
-      dispatch(onLogin({name: data.name, uid: data.uid}))
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
       
     } catch (error) {
       console.log(error)
@@ -26,15 +26,14 @@ export const useAuthStore = () => {
   }
 
   const startRegister = async ({registerName: name, email, password }) => {
-    const { data } = await calendarAPI.post('/auth/new', {name, email, password })
     try {
+      const { data } = await calendarAPI.post('/auth/new', {name, email, password })
       localStorage.setItem('token', data.token);
       localStorage.setItem('token-init-date', new Date().getTime());
       dispatch(onLogin({name: data.name, uid: data.uid}))
     } catch (error) {
       console.log(error);
-      console.log(data.msg)
-      dispatch(onLogout(data.msg));
+      dispatch(onLogout(error.response.data?.msg));
     }
   }
 
